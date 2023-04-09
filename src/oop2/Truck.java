0a6e166162b83a1e08ea3f56f1b560e48cd3f24b
admin <region29@bk.ru>
2023-04-10 00:46:42 +0300

    ДЗ

---
+++ src/oop2/Truck.java
@@ -0,0 +1,30 @@
+package oop2;
+
+public class Truck extends Car implements Maintainable {
+
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+
+    public void checkTrailer() {
+        System.out.println(getModelName() + " Проверяем прицеп");
+    }
+
+    @Override
+    public void service() {
+        updateTyre();
+        checkEngine();
+        checkTrailer();
+    }
+
+
+    }
+
+
+
+
+
+
+
+
---
+++ src/oop2/serviceStation.java
@@ -0,0 +1,11 @@
+package oop2;
+
+public class serviceStation {
+
+    public void check(Maintainable vehicle) {
+        vehicle.service();
+
+
+        // questions:
+    }
+}
